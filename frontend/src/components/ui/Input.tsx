34d483d862c2c94750531a6e094894b1afb7c565
lokesh-manneti <mannetilokesh@gmail.com>
2025-12-15 21:28:06 +0530

    fixing issues found in dev tools

--- frontend/src/components/ui/Input.tsx
+++ frontend/src/components/ui/Input.tsx
@@ -1,7 +1,8 @@
-import * as React from "react";
-import { cn } from "@/lib/utils";
+import * as React from "react"
+import { cn } from "@/lib/utils"
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -13,11 +14,12 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           className
         )}
         ref={ref}
+        suppressHydrationWarning={true} // <--- ADD THIS LINE
         {...props}
       />
-    );
+    )
   }
-);
-Input.displayName = "Input";
+)
+Input.displayName = "Input"
 
-export { Input };
+export { Input }
